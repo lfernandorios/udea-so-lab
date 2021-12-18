884933c834d9d7f263d6281e82e243501029407e
Joan Rivas <dairo.rivas@udea.edu.co>
2021-12-17 23:02:20 -0500

    Finalizacion Etapa 4 comprobacion de parametros ingresados

--- Taller01/psinfo.c
+++ Taller01/psinfo.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include "psinfo.h"
 #include <stdlib.h>
+#include <ctype.h>
 
 
 #define LONGITUD_MAXIMA_CADENA 1000
@@ -15,11 +16,16 @@ int main(int argc, char* argv[])
 	ps_data *datos;	
 	datos=(ps_data*)malloc(sizeof(ps_data));
 	if(argc==2){
+		validar_num(argv[1]);
 		leer_archivo(argv[1],datos);	
 		imprimir_datos(datos);
 	}
 	
 	if (argc>=3){
+		
+		for(int i=0;i<(argc-2);i++){
+			validar_num(argv[i+2]);		
+		}
 		int tipo_param=comprobar_parametro(argv[1]);
 		parametro_especial(tipo_param, argc, argv,datos);
 			
@@ -37,13 +43,12 @@ void parametro_especial(int tp, int argc, char *argv[], ps_data *datos){
 	switch(tp){
 			
 			//caso 1 si es -l
-			case 1: 
-				//Hacer un for por cada parametro argc-1
-				printf("---!Informacion Recolectada!---\n");
-				for(int i=1; i<(argc-1);i++){
-					leer_archivo(argv[i+1],datos);
+			case 1: 				
+				printf("---!Informacion!---\n");
+				for(int i=0; i<(argc-2);i++){
+					leer_archivo(argv[i+2],datos);
 					imprimir_datos_b(datos);
-					printf("-------------------------------\n");
+					printf("------------------\n");
 				}
 				
 				break;
@@ -69,11 +74,15 @@ void parametro_especial(int tp, int argc, char *argv[], ps_data *datos){
 				printf("Archivo de salida generado: %s\n",buffer);
 				
 				break;
+			default: printf("Parametro '%s' invalido, digite un numero de ID de proceso, o ingrese -l o -r seguido del ID de proceso\n ",argv[1]);
+			printf("Ejemplo: ./psinfo -l 2031 3520\n ./psinfo -r 2031 3520 \n ./psinfo 2031\n");
+			exit(EXIT_SUCCESS);
 		
 		}
 	
 }
 
+//Funcion para escribir un archivo con la info del proceso
 void imprimir_reporte(ps_data *d,char *b){
 	   	
 	const char* filename = b;
@@ -99,8 +108,10 @@ void leer_archivo(char *pid,ps_data *datos){
 	sprintf(statFileName, "/proc/%s/status", pid);
 	
 	FILE *fd = fopen(statFileName, "r");
-	if (fd == NULL)
-		return perror("No puedo encontrar el proceso especificado");
+	if (fd == NULL){
+		printf("\nNo se encontro el proceso especificado\n");
+		exit(EXIT_SUCCESS);
+	}
     
 	char bufer[LONGITUD_MAXIMA_CADENA];
 	char *linea, *campo, *valor;
@@ -128,6 +139,7 @@ void leer_archivo(char *pid,ps_data *datos){
 	fclose(fd);
 }
 
+//Funcion guarda las variables leidas del archivo
 void guardar_variables(char* c, char* v, ps_data *d){
 	if(strcmp(c,"Name")==0) {strcpy(d->name,v); }
 	else if(strcmp(c,"State")==0) {strcpy(d->state,v); }
@@ -144,15 +156,14 @@ void guardar_variables(char* c, char* v, ps_data *d){
 }
 
 
-
-
+//Funcion devuelve 1 o 2 dependiendo del parametro
 int comprobar_parametro(char* p){
 	if(strcmp(p,"-l")==0) {return 1;}
 	if(strcmp(p,"-r")==0) {return 2;}
 	else{ return 0;}
 }
 
-
+//Funcion para imprimir los datos basico de un proceso
 void imprimir_datos_b(ps_data *d){
 
 	printf("Pid: %s\n", d->pid);
@@ -161,7 +172,7 @@ void imprimir_datos_b(ps_data *d){
 
 }
 
-
+//Funcion para imprimir todos los datos importantes del proceso
 void imprimir_datos(ps_data *d){
 
 	printf("Nombre del proceso: %s\n", d->name);
@@ -170,6 +181,27 @@ void imprimir_datos(ps_data *d){
 	printf("Tamano de la memoria en la region TEXT: %s\n", d->vmExe);
 	printf("Tamano de la memoria en la region DATA: %s\n", d->vmData);
 	printf("Tamano de la memoria en la region STACK: %s\n", d->vmStk);
-	printf("Numero de cambios de contexto realizados (voluntarios - no voluntarios): %s - %s\n", d->voluntario, d->noVoluntario);	
+	printf("Numero de cambios de contexto realizados (voluntarios - no voluntarios): %s - %s\n", d->voluntario, d->noVoluntario);
 
 }
+
+//Funcion valida que los parametros ingresado para el ID del proceso si sean correctos
+void validar_num(char n[50]){
+	int i=0, j;
+	j=strlen(n);
+	while(i<j ){
+		if(isdigit(n[i])!=0){
+			i++;			
+		}else{			
+			printf("Parametro '%s' invalido, digite un numero de ID de proceso, o ingrese -l o -r seguido del ID de proceso\n ",n);
+			printf("Ejemplo: ./psinfo -l 2031 3520\n ./psinfo -r 2031 3520 \n ./psinfo 2031\n");
+			exit(EXIT_SUCCESS);
+			
+		}
+	}
+	
+}
+
+
+
+
